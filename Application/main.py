88cab1be471ba581e6714383aa306746f30d729c
Mehar Goli <goli.mehar@gmail.com>
2022-11-06 10:38:55 -0500

    Updated code to work with yolo's model running files

--- Application/main.py
+++ Application/main.py
@@ -1,46 +1,21 @@
-import cv2, torch,time,json
+import cv2, torch,time, sys
 import numpy as np
 from preprocess_image import preprocess
 
+sys.path.append("../../ScottySeat/Model_Development/yolov5")
+from detect import run
+
+
 def main():
 
-    #argv camera path, model path, output path,
-    camera = cv2.VideoCapture(1, cv2.CAP_AVFOUNDATION)
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5l')
+    #camera = cv2.VideoCapture(1, cv2.CAP_AVFOUNDATION)
+    # #img = camera.read()
 
-    # while True:
-        #img = camera.read()
-
-    img = cv2.imread('insert_path_here/ScottySeat/Testing Images/IMG_4575 Large.jpeg')
+    img = cv2.imread('../../ScottySeat/Testing Images/IMG_4575 Large.jpeg')
     img = preprocess(img)
-    out = model(img)
+    cv2.imwrite('../../ScottySeat/b3/scottyseats/data/data.jpg', img)
 
-    results = out.pandas().xyxy[0].values
+    out = run(weights='yolov5s.pt',source='../../ScottySeat/b3/scottyseats/data/data.jpg'\
+        , save_txt=True, nosave=True, project='../../ScottySeat/b3/scottyseats', name='data', exist_ok=True)
 
-    results_chairs = [res for res in results if res[-1] == 'chair']
-    results_people = [res for res in results if res[-1] == 'person']
-    results_table = [res for res in results if res[-1] == 'dining table']
-   
-    #np.savetxt(results_chairs)
-    width, height = img.shape
-    results = results_chairs + results_people + results_table
-
-    with open('insert_path_here/ScottySeat/b3/scottyseats/data/data.txt', 'w') as f:
-        for a in results:
-            f.write(str(a[-2]))
-            f.write(str(' '))
-            f.write(str(float(a[0])/height))
-            f.write(str(' '))
-            f.write(str(float(a[1])/width))
-            f.write(str(' '))
-            f.write(str(float(a[2])/height))
-            f.write(str(' '))
-            f.write(str(float(a[3])/width))
-            f.write(str('\n'))
-
-            print(a)
-        
-#         time.sleep(60)
-
-main()
 
